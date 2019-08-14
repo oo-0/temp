446ea1469773514f39bb191e3b4599ca80052bdc
oo-0 <anshulgangu@gmail.com>
2019-08-14 12:49:32 +0530

    Add function for addition

--- 1.cpp
+++ 1.cpp
@@ -1,12 +1,23 @@
 #include <iostream.h>
 using namespace std;
-void add();
+void add(int,int);
 void sub();
 int main(){
 	int i;
-	if(i==0){
+	cout<<"Enter 1 for addition ";
+	cin>>i;
+	int a,b;
+	if(i==1){
+		cout<<"Enter the numbers "<<endl;
+		cin>>a>>b;
+		add(a,b);
 	}
 	else{
+		
 	}
 	return 0;
+}
+
+void add(int a,int b){
+return a+b;
 }
